362b75fee97190e038323c9343afc234d3efab1c
Akshay B <beatle02@gmail.com>
2023-06-25 22:59:50 +0530

    batch scripts

---
+++ get_ranking.bat
@@ -0,0 +1,8 @@
+
+echo off
+echo Running the script to rank the shares...
+python async.py
+
+echo:
+
+pause
---
+++ get_stock_info.bat
@@ -0,0 +1,8 @@
+
+echo off
+echo Running the script to fetch share values...
+python get-stock-info-v2.py
+
+echo:
+
+pause
