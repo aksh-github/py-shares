80fc4fc64ac138ec63983ba6dad2624952fe6b68
Akshay B <beatle02@gmail.com>
2024-12-02 17:59:16 -0500

    Get yearly data, calculate month's max for all 12 months and write to excel

--- 27-oct-24/simple-calc-27oct24.py
+++ 27-oct-24/simple-calc-27oct24.py
@@ -19,24 +19,23 @@ def send_telegram_message(message):
     try:
         if not TOKEN or not CHAT_ID:
             raise ValueError("TOKEN or CHAT_ID not found in config file")
+        url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+        data = {'chat_id': CHAT_ID, 'text': message}
+
+        # print(url, data)
+        
+        # Send the request
+        response = requests.post(url, json=data)
+
+        # Check if the message was sent successfully
+        if response.status_code == 200:
+            print("Message sent successfully!")
+        else:
+            print("Error sending message:")
+            print(response.text)
     except ValueError as e:
         print(e)
-        exit()
-    
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    data = {'chat_id': CHAT_ID, 'text': message}
-
-    # print(url, data)
-    
-    # Send the request
-    response = requests.post(url, json=data)
-
-    # Check if the message was sent successfully
-    if response.status_code == 200:
-        print("Message sent successfully!")
-    else:
-        print("Error sending message:")
-        print(response.text)
+        # exit()
 
 def get_top_stock_data(stock_file_path):
 
@@ -44,9 +43,8 @@ def get_top_stock_data(stock_file_path):
     # stock_file_path = './27-oct-24/stock_names.txt'
 
     # Define the file path for the output CSV file
-    output_file_path = './27-oct-24/stock_data.csv'
-    output_summary_path = './27-oct-24/stock_summary.csv'
     output_summary_path_xl = "./27-oct-24/stock_summary" + '-' + str(uuid.uuid4())[:8] + ".xlsx"
+    output_eom_path_xl = "./27-oct-24/eom_closes" + '-' + str(uuid.uuid4())[:8] + ".xlsx"
 
     # Read the stock names from the text file
     try:
@@ -65,6 +63,9 @@ def get_top_stock_data(stock_file_path):
     # Create an empty DataFrame to store the stock data
     stock_data = pd.DataFrame(columns=['Stock', 'LYr Price', 'Top Price', 'Curr Price', 'LYr % Chg', 'Curr % Chg', 'LYr-Curr % Chg'])
 
+    # eomAllStockData = pd.DataFrame(columns=['Stock', 'Date1', 'Date2', 'Date3', 'Date4', 'Date5', 'Date6', 'Date7', 'Date8', 'Date9', 'Date10', 'Date11', 'Date12'])
+    dfs = []
+
     # Fetch the data for each stock and extract the required prices
     for stock_name in stock_names:
         try:
@@ -74,6 +75,9 @@ def get_top_stock_data(stock_file_path):
             if data is None or data.empty:
                 print(f"Error fetching data for {stock_name}: empty or null data")
                 continue
+
+            # 1. % change (speed calculation)
+
             last_year_price = round(data['Close'].iloc[0] if data['Close'].iloc[0] is not None else 0, 2)
             top_price = round(data['Close'].max() if data['Close'].max() is not None else 0)
             current_price = round(data['Close'].iloc[-1] if data['Close'].iloc[-1] is not None else 0)
@@ -89,10 +93,34 @@ def get_top_stock_data(stock_file_path):
             if top_price > current_price:
                 current_percent_change = -current_percent_change
             
-            stock_data = pd.concat([stock_data, pd.DataFrame({'Stock': [stock_name], 'LYr Price': [last_year_price], 'Top Price': [top_price], 'Curr Price': [current_price], 'LYr % Chg': [LY_percent_change], 'Curr % Chg': [current_percent_change] , 'LYr-Curr % Chg': [last_Curr_percent_change]})], ignore_index=True)
+            stock_data = pd.concat([stock_data, pd.DataFrame({'Stock': [stock_name.replace('.NS', '').replace('.BO', '')], 'LYr Price': [last_year_price], 'Top Price': [top_price], 'Curr Price': [current_price], 'LYr % Chg': [LY_percent_change], 'Curr % Chg': [current_percent_change] , 'LYr-Curr % Chg': [last_Curr_percent_change]})], ignore_index=True)
+
+            # end % change (speed calculation)
+
+
+            # 2. Get monthly close price
+
+            eomStockData = data.resample('ME').max()
+            # print(eomStockData)
+            
+            # Select only the 'Close' column
+            closing_prices = eomStockData.loc[:, ['Close']].round(2)
+            
+            # Rename the column to include the stock symbol
+            closing_prices = closing_prices.rename(columns={'Close': stock_name.replace('.NS', '').replace('.BO', '')})
+            
+            # Merge the closing prices with the existing DataFrame
+            
+
+            # eomAllStockData = pd.concat([eomAllStockData, closing_prices], axis=1)
+            # print(closing_prices)
+
+            dfs.append(closing_prices)
+
+            # end Get monthly close price
+
         except Exception as e:
             print(f"Error fetching data for {stock_name}: {str(e)}")
-
 
     # Print the stock data
     # print(stock_data)
@@ -112,7 +140,28 @@ def get_top_stock_data(stock_file_path):
     print(top5.to_string(index=False))
 
     # Send the data as a message to Telegram
-    send_telegram_message(top5[["Stock", "Curr Price", "LYr-Curr % Chg"]].to_string(index=False))
+    try:
+        send_telegram_message(top5[["Stock", "Curr Price", "LYr-Curr % Chg"]].to_string(index=False))
+    except Exception as e:
+        print(f"Data was not sent to Telegram: {str(e)}")
+
+    # monthly data
+    # print(dfs)
+
+    # Concatenate the list of DataFrames
+    df = pd.concat(dfs, axis=1)
+
+    # Convert the date index to 'MMM-YY' format
+    df.index = df.index.strftime('%b-%y')
+
+    # Transpose the DataFrame
+    df = df.T
+
+    # Write the DataFrame to an Excel file
+    df.to_excel(output_eom_path_xl, header=True, index=True)
+
+    # end monthly data
+
 
     # Save the data to an Excel file
     # try:
